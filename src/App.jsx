111f279a4d4a8d4d6ad18315d8f790fa2f93910c
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-04-13 12:42:45 -0500

    Restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-import Sidebar from './components/SideBar.jsx';
-
 function App() {
-	return (
-		<>
-			<Sidebar />
-		</>
-	);
+	return <></>;
 }
 
 export default App;
