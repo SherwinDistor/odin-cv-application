4200af57115def0905e179fba5f84191cbfb9165
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-09-06 17:33:05 -0500

    Adding state to personal details

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
+import InputCard from './components/InputCard';
+
 function App() {
+	const [personalDetails, setPersonalDetails] = useState({
+		name: 'Joe Shmo',
+		email: 'example@example.com',
+		number: '(555)555-5555',
+		location: 'Austin, TX',
+		link: 'linkedin.com/joeshmoe1',
+	});
+
 	return (
 		<>
 			<div className='bg-zinc-200 p-4 h-full flex gap-4'>
 				<div>
-					<div className='bg-zinc-100 p-4 rounded-lg min-w-80'>
+					<InputCard>
 						<h1 className='text-xl font-bold mb-2'>Personal Details</h1>
 						<h2 className='text-md font-medium'>Full Name</h2>
 						<input
 							className='bg-zinc-200 p-1 rounded-lg w-full mb-2'
 							type='text'
-							name=''
-							id=''
+							name='name'
+							id='name'
+							onChange={(e) =>
+								setPersonalDetails({ ...personalDetails, name: e.target.value })
+							}
+							value={personalDetails.name}
 						/>
 						<h2 className='text-md font-medium'>Email</h2>
 						<input
@@ -18,6 +33,13 @@ function App() {
 							type='text'
 							name=''
 							id=''
+							onChange={(e) =>
+								setPersonalDetails({
+									...personalDetails,
+									email: e.target.value,
+								})
+							}
+							value={personalDetails.email}
 						/>
 						<h2 className='text-md font-medium'>Phone Number</h2>
 						<input
@@ -25,22 +47,51 @@ function App() {
 							type='text'
 							name=''
 							id=''
+							onChange={(e) =>
+								setPersonalDetails({
+									...personalDetails,
+									number: e.target.value,
+								})
+							}
+							value={personalDetails.number}
 						/>
-						<h2 className='text-md font-medium'>Address</h2>
+						<h2 className='text-md font-medium'>Location</h2>
 						<input
 							className='bg-zinc-200 p-1 rounded-lg w-full mb-2'
 							type='text'
 							name=''
 							id=''
+							onChange={(e) =>
+								setPersonalDetails({
+									...personalDetails,
+									location: e.target.value,
+								})
+							}
+							value={personalDetails.location}
 						/>
-					</div>
+						<h2 className='text-md font-medium'>LinkedIn</h2>
+						<input
+							className='bg-zinc-200 p-1 rounded-lg w-full mb-2'
+							type='text'
+							name=''
+							id=''
+							onChange={(e) =>
+								setPersonalDetails({ ...personalDetails, link: e.target.value })
+							}
+							value={personalDetails.link}
+						/>
+					</InputCard>
+					<InputCard>Hello</InputCard>
 				</div>
 				<div className='w-full'>
 					<div className='bg-zinc-100 w-[210mm] h-[297mm]'>
 						<div>
-							<h1>Sherwin John Distor</h1>
-							<p>Location | (888)888-8888 | Email address</p>
-							<a href=''>LinkedIn link</a>
+							<h1>{personalDetails.name}</h1>
+							<p>
+								{personalDetails.location} | {personalDetails.number} |{' '}
+								{personalDetails.email}
+							</p>
+							<a href=''>{personalDetails.link}</a>
 						</div>
 					</div>
 				</div>
---
+++ src/components/InputCard.jsx
@@ -0,0 +1,7 @@
+function InputCard({ children }) {
+	return (
+		<div className='bg-zinc-100 p-4 rounded-lg min-w-80 mb-4'>{children}</div>
+	);
+}
+
+export default InputCard;
